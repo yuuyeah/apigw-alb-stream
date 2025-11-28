806e7b915b06a5aef0730a95ae4d5d7bf7b590e6
yuymt <yuymt@amazon.co.jp>
2025-11-28 21:26:18 +0900

    Initial commit

---
+++ test/apigw-alb-stream.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ApigwAlbStream from '../lib/apigw-alb-stream-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/apigw-alb-stream-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ApigwAlbStream.ApigwAlbStreamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
