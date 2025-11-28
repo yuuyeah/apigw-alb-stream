112813a1a0164a7c3b75efe16dc63cffa30483ff
yuymt <yuymt@amazon.co.jp>
2025-11-28 21:55:22 +0900

    feat: implement private ECS deployment with ALB

--- lib/apigw-alb-stream-stack.ts
+++ lib/apigw-alb-stream-stack.ts
@@ -1,16 +1,52 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { EcsAppConstruct } from './constructs/ecs-app-construct';
 
 export class ApigwAlbStreamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC (Private Subnetのみ、NAT Gatewayなし)
+    const vpc = new ec2.Vpc(this, 'Vpc', {
+      maxAzs: 2,
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApigwAlbStreamQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // VPC Endpoints
+    vpc.addInterfaceEndpoint('EcrDockerEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+    });
+    vpc.addInterfaceEndpoint('EcrEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.ECR,
+    });
+    vpc.addGatewayEndpoint('S3Endpoint', {
+      service: ec2.GatewayVpcEndpointAwsService.S3,
+    });
+    vpc.addInterfaceEndpoint('CloudWatchLogsEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+    });
+    vpc.addInterfaceEndpoint('BedrockRuntimeEndpoint', {
+      service: ec2.InterfaceVpcEndpointAwsService.BEDROCK_RUNTIME,
+    });
+
+    // ECS App
+    const ecsApp = new EcsAppConstruct(this, 'EcsApp', {
+      vpc,
+      appPath: './app',
+    });
+
+    // Outputs
+    new cdk.CfnOutput(this, 'ALBDnsName', {
+      value: ecsApp.alb.loadBalancerDnsName,
+      description: 'Private ALB DNS Name',
+    });
   }
 }
---
+++ lib/constructs/ecs-app-construct.ts
@@ -0,0 +1,94 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+export interface EcsAppConstructProps {
+  vpc: ec2.IVpc;
+  appPath: string;
+}
+
+export class EcsAppConstruct extends Construct {
+  public readonly service: ecs.FargateService;
+  public readonly alb: elbv2.ApplicationLoadBalancer;
+
+  constructor(scope: Construct, id: string, props: EcsAppConstructProps) {
+    super(scope, id);
+
+    const { vpc, appPath } = props;
+
+    // ECS Cluster
+    const cluster = new ecs.Cluster(this, 'Cluster', {
+      vpc,
+      containerInsights: true,
+    });
+
+    // Task Role
+    const taskRole = new iam.Role(this, 'TaskRole', {
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+    taskRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ['bedrock:InvokeModelWithResponseStream', 'bedrock:InvokeModel'],
+        resources: ['*'],
+      }),
+    );
+
+    // Task Definition
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'TaskDef', {
+      memoryLimitMiB: 512,
+      cpu: 256,
+      taskRole,
+    });
+
+    const container = taskDefinition.addContainer('app', {
+      image: ecs.ContainerImage.fromAsset(appPath),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'app' }),
+      environment: {
+        AWS_DEFAULT_REGION: cdk.Stack.of(this).region,
+      },
+    });
+
+    container.addPortMappings({
+      containerPort: 8080,
+      protocol: ecs.Protocol.TCP,
+    });
+
+    // Private ALB
+    this.alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
+      vpc,
+      internetFacing: false,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    const listener = this.alb.addListener('Listener', {
+      port: 80,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+    });
+
+    // Fargate Service
+    this.service = new ecs.FargateService(this, 'Service', {
+      cluster,
+      taskDefinition,
+      desiredCount: 1,
+      assignPublicIp: false,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_ISOLATED },
+    });
+
+    listener.addTargets('ECS', {
+      port: 8080,
+      protocol: elbv2.ApplicationProtocol.HTTP,
+      targets: [this.service],
+      healthCheck: {
+        path: '/health',
+        interval: cdk.Duration.seconds(30),
+        timeout: cdk.Duration.seconds(5),
+        healthyThresholdCount: 2,
+        unhealthyThresholdCount: 3,
+      },
+      deregistrationDelay: cdk.Duration.seconds(30),
+    });
+  }
+}
